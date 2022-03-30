70dfe4ac0a74d6a5792d43371b8818bbdb036692
user <user@A401-PC46>
2022-03-30 15:16:53 +0800

    加入專案檔案。

---
+++ 20220330-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220330_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Random R =new Random();
+            Console.Write("n=");
+            int sum = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 50; i++)
+            {
+                int temp = R.Next(1,100);
+                Console.WriteLine(temp);
+                sum += temp;
+            }
+            Console.WriteLine(((double)sum) / n);
+            Console.ReadLine();
+                
+                
+               
+            Console.WriteLine(R.Next(1,100));
+            Console.WriteLine($"你輸出入的");
+            Console.ReadLine();
+        }
+    }
+}
